d176bd107e44271f45759b08a6c259336298102f
Jennifer Chow <jennego@gmail.com>
2022-01-05 22:49:27 -0800

    add font file as font-face

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,16 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Pixel Ponies</h1>
+    <h1 className="heading">Pixel Ponies</h1>
     <p> A collection of equestrian-themed and related apps and games.</p>
     <p>
       Just because you spend a lot of time in a barn doesn't mean your tools
       need to be from a cave!
     </p>
+    <p> WASD or arrow keys to move horse!</p>
+
+    <h1>Stable </h1>
+    <p>Change your horse colours, name your horse.</p>
   </Layout>
 )
 
